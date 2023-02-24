3ae3efb913db16230c31aea718f1bc31703ae356
Danil <sk10azik@gmail.com>
2023-02-24 21:59:21 +0300

    add config

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/sk10az/butler-bot
 
 go 1.20
+
+require (
+	github.com/gotd/td v0.76.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package butler_bot
+
+import (
+	"log"
+)
+
+type QueueItem struct {
+}
+
+func main() {
+	log.Println("Starting...")
+	config, err := loadConfig()
+	if err != nil {
+		panic(err)
+	}
+
+}
